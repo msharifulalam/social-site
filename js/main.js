21720d1432f91292148699c56eb1162490e69c28
msharifulalam <info.tusarnill@gmail.com>
2019-05-20 02:03:57 +0600

    social-site theme uploaded

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+
+	// amout
+	$("#inc").click(function(){
+	    $("#amountSpan").text( Number($("#amountSpan").text()) + 1 );
+	});
+	$("#dec").click(function(){
+	    $("#amountSpan").text( Number($("#amountSpan").text()) - 1 );
+	});
+
+	// flat toggle
+	$('.flat-toggle').on('click', function() {
+        $(this).toggleClass('off');
+    });
+
+    $('.flat-toggle').on('click', function() {
+        $('.private').toggle(function(){
+            $('.private').addClass('public');
+            $('.public').removeClass('public');
+        });
+    });
+
+	// slick nav
 	$('#nav').slicknav();
 });
 
